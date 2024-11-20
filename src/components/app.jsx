a41f816a7e275b718c9019a863c113506175ae51
Aarushi Singh <singhaarushi05@gmail.com>
2024-11-20 13:33:51 +0000

    updated

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,26 +1,27 @@
 import React, { useState } from 'react';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./login";
 import Nav from "./nav";
 import ViewAll from "./viewAll";
 import SendMoney from "./sendMoney";
 import TransactionHistory from "./transactions";
-import Home from "./home";
 import NewCustomer from "./newcustomer";
 import EditCustomer from "./editcustomer";
 import Transfer from "./sendmoney2";
+import Home from "./home";
+
 
 const AppComponent = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false); 
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     const handleLoginSuccess = () => {
-        setIsLoggedIn(true); 
+        setIsLoggedIn(true);
     };
 
     return (
         <div className="App">
             {isLoggedIn ? (
-                <HomePage /> 
+                <HomePage />
             ) : (
                 <Login onLoginSuccess={handleLoginSuccess} /> 
             )}
@@ -28,19 +29,32 @@ const AppComponent = () => {
     );
 };
 
-function App(){
-    return (<div>
-    <Router>
-    <Nav/>
-        <Route path="/view" component={ViewAll} exact/>
-        <Route path="/transfer" component={SendMoney} exact/>
-        <Route path="/" component={Home} exact/>
-        <Route path="/transaction" component={TransactionHistory} exact/>
-        <Route path="/new" component={NewCustomer} exact/>
-        <Route path="/update" component={EditCustomer}/>
-        <Route path="/sendmoney" component={Transfer} />
-    </Router>
-    </div>);
+function App() {
+    return (
+        <div>
+            <Router>
+                <Nav />
+                <Route path="/view" component={ViewAll} exact />
+                <Route path="/transfer" component={SendMoney} exact />
+                <Route path="/" component={HomePage} exact />  {/* Route to HomePage */}
+                <Route path="/transaction" component={TransactionHistory} exact />
+                <Route path="/new" component={NewCustomer} exact />
+                <Route path="/update" component={EditCustomer} />
+                <Route path="/sendmoney" component={Transfer} />
+            </Router>
+        </div>
+    );
 }
+
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to the Banking System</h1>
+        </div>
+    );
+};
+
+export { HomePage }; // Export the HomePage component
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from "./components/app";
 
-ReactDOM.render(<App/> , document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+    ReactDOM.render(<App /> , rootElement);
+}
+else {
+    console.error('Root element not found');
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+)
